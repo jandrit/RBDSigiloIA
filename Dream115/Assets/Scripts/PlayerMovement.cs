e519ced4b814931f0c97d4f1b23ad15e3a0b41e4
jandrit <al361652@uji.es>
2019-09-24 22:50:16 +0200

    Summit inicial
    Hola camaradas de introducción al anarquismo, como el anarquismo es una mierda, mejor hacemos un juego de sigilo.
    
    Esta creado el unity (obvio), metido el modelo y animaciones de aythami, animado con el animator y hecho el movimiento. Cuando meta lo de lanzar bolas hare q colisionen con el personaje y programaré el daño.
    
    Mañana cuando Àlex lo suelte haré lo del menú de pausa y miraré lo del enemigo que lanza cosas.
    
    Que os vaya bien, besitos.

---
+++ Dream115/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 4;
+    float rotSpeed = 160;
+    float rot = 0f;
+    float gravity = 8;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    CharacterController controller;
+    Animator anim;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    moveDirection = new Vector3(0, 0, 1);
+                    moveDirection *= speed * 2;
+                    moveDirection = transform.TransformDirection(moveDirection);
+                    anim.SetFloat("Speed", speed * 2);
+                } else
+                {
+                    moveDirection = new Vector3(0, 0, 1);
+                    moveDirection *= speed;
+                    moveDirection = transform.TransformDirection(moveDirection);
+                    anim.SetFloat("Speed", speed);
+                }
+            }
+
+            if (Input.GetKeyUp (KeyCode.W))
+            {
+                moveDirection = new Vector3(0, 0, 0);
+                anim.SetFloat("Speed", 0);
+            }
+        }
+        rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, rot, 0);
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
