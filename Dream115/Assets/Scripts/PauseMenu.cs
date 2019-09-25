65a9477978f3b6f8c2fcb0c80873d45cfa057834
jandrit <al361652@uji.es>
2019-09-25 21:58:32 +0200

    Menú de pausa básico hecho
    Se activa con Escape y van los botones de resume y quit. Los botones de menú y opciones cuando tengamos con que llenar los llenamos.
    
    Feliz noche de miércoles

---
+++ Dream115/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+
+    public void Options()
+    {
+        //Aquí metemos todas las opciones
+    }
+
+    public void LoadMenu()
+    {
+        //Aquí cargamos el menú principal
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
